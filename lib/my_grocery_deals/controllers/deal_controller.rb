9dcd326922bd141c6e3fecda97002020ca0f847a
Dennis O'Connor <doconnor@mygrocerydeals.com>
2015-10-20 15:14:57 -0400

    Added removed related method

--- lib/my_grocery_deals/controllers/deal_controller.rb
+++ lib/my_grocery_deals/controllers/deal_controller.rb
@@ -9,56 +9,6 @@ module MyGroceryDeals
         @x_api_key = x_api_key;
     end
 
-    # return a list of similar deals to the one specified
-    # @param [Numeric] id Required parameter: TODO: type description here
-    # @param [String] distance Optional parameter: Radius to search
-    # @param [String] ip Optional parameter: End user's ip address
-    # @param [Numeric] latitude Optional parameter: End user's latitude
-    # @param [Numeric] longitude Optional parameter: End user's longitude
-    # @param [String] zip Optional parameter: End user's zip code
-    # @return DealRelatedResponse response from the API call
-    def get_deal_related_by_id id, distance: nil, ip: nil, latitude: nil, longitude: nil, zip: nil
-      # the base uri for api requests
-      query_builder = Configuration::BASE_URI.dup % [@version]
-
-      # prepare query string for API call
-      query_builder << "/deal/{id}/related"
-
-      # process optional query parameters
-      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
-        "id" => id,
-      }
-
-      # process optional query parameters
-      query_builder = APIHelper.append_url_with_query_parameters query_builder, {
-        "distance" => distance,
-        "ip" => ip,
-        "latitude" => latitude,
-        "longitude" => longitude,
-        "zip" => zip,
-      }
-
-      # validate and preprocess url
-      query_url = APIHelper.clean_url query_builder
-
-      # prepare headers
-      headers = {
-        "user-agent" => "APIMATIC 2.0",
-        "accept" => "application/json",
-        "X-Api-Key" => @x_api_key
-      }
-
-      # invoke the API call request to fetch the response
-      response = Unirest.get query_url, headers:headers
-
-      #Error handling using HTTP status codes
-      if !(response.code.between?(200,206)) # [200,206] = HTTP OK
-        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
-      end
-
-      response.body
-    end
-
     # lookup a specific deal by identifier
     # @param [Numeric] id Required parameter: TODO: type description here
     # @return DealEntity response from the API call
